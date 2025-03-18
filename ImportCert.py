dacbe3f7d68b14815f19a1f76b860c0ad04cfae6
RRENN02 <148074001+RRENN02@users.noreply.github.com>
2025-03-18 10:03:19 +0800

    Completed base functions

--- ImportCert.py
+++
@@ -1,4 +0,0 @@
-import os
-
-filepath = input('Enter the path of the certificate: ')
-os.cd(os.system('certutil -addstore -f "Root" ' + filepath))
--- index.py
+++ index.py
@@ -1,25 +1,34 @@
 from customtkinter import *
+import os
 
 app = CTk()
 app.title("DCC")
 app.geometry("500x400")
+set_appearance_mode("dark")
 
-file = ""
+# Store values
+# cert_path = ""
+# format_path = ""
 
 def open_cert():
     filename = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg")])
     certLabel.configure(text=filename)
+    cert = filename
     return filename
 
-def open_xml():
+def open_format():
     filename = filedialog.askopenfilename(filetypes=[("XML files", "*.xml")])
     formatLabel.configure(text=filename)
+    format = filename
     return filename
 
-# def load():
-#     with open(textField.get()) as file:
-#         txt.SetValue(file.read())
-
+def export_dcc():
+    dcc = "DCC.xml"
+    dccLabel.configure(text="DCC successfuly generated named " + dcc)
+    with open(dcc, 'w') as file:
+        file.write("Certificate path: " + certLabel.cget("text") + "\n")
+        file.write("Format path: " + formatLabel.cget("text") + "\n")
+    return dcc
 
 # Title label
 titleLabel = CTkLabel(master=app, text="DCC Generator", font=("Arial", 24))
@@ -39,7 +48,7 @@ certLabel.place(relx=0.5, rely=0.40, anchor="center")
 # import button for xml
 importFormat = CTkButton(master=app, text="Select Calibration Format XML", corner_radius=15,
                     fg_color="#4158D0", hover_color="#C850C0", border_color="#FFCC70",
-                    border_width=2)
+                    border_width=2, command=open_format)
 importFormat.place(relx=0.5, rely=0.50, anchor="center")
 
 # Textlabel for Calibration Format XML
@@ -49,7 +58,7 @@ formatLabel.place(relx=0.5, rely=0.60, anchor="center")
 # export button for DCC
 exportDCC = CTkButton(master=app, text="Export DCC", corner_radius=15,
                     fg_color="#4158D0", hover_color="#C850C0", border_color="#FFCC70",
-                    border_width=2)
+                    border_width=2, command=export_dcc)
 exportDCC.place(relx=0.5, rely=0.70, anchor="center")
 
 # Textlabel for DCC
