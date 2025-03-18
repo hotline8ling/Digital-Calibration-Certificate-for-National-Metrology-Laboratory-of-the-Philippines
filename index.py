468671acf6c8e144bb0ee31e73a61090bd14e56d
RRENN02 <148074001+RRENN02@users.noreply.github.com>
2025-03-18 14:28:54 +0800

    Update Format

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from customtkinter import *
+from PIL import Image
 import os
 
 app = CTk()
@@ -6,64 +7,66 @@ app.title("DCC")
 app.geometry("500x400")
 set_appearance_mode("dark")
 
-# Store values
-# cert_path = ""
-# format_path = ""
-
+# Functions
 def open_cert():
     filename = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg")])
     certLabel.configure(text=filename)
-    cert = filename
     return filename
 
 def open_format():
     filename = filedialog.askopenfilename(filetypes=[("XML files", "*.xml")])
     formatLabel.configure(text=filename)
-    format = filename
     return filename
 
 def export_dcc():
     dcc = "DCC.xml"
-    dccLabel.configure(text="DCC successfuly generated named " + dcc)
+    dccLabel.configure(text=dcc + " successfully generated")
     with open(dcc, 'w') as file:
         file.write("Certificate path: " + certLabel.cget("text") + "\n")
         file.write("Format path: " + formatLabel.cget("text") + "\n")
     return dcc
 
 # Title label
-titleLabel = CTkLabel(master=app, text="DCC Generator", font=("Arial", 24))
-titleLabel.place(relx=0.5, rely=0.10, anchor="center")
+titleLabel = CTkLabel(master=app, text="DCC Generator", font=("Arial", 32))
+titleLabel.place(relx=0.15, rely=0.05)
+
+# Textlabel for Import Calibration Certificate image
+importCertL = CTkLabel(master=app, text="Select a Calibration Certificate Image", font=("Arial", 12))
+importCertL.place(relx=0.15, rely=0.20)
 
 # import button for image
-importCert = CTkButton(master=app, text="Select Calibration Certificate Image", corner_radius=15, 
+importCert = CTkButton(master=app, text="Upload File", corner_radius=10, 
                     fg_color="#4158D0", hover_color="#C850C0", border_color="#FFCC70",
                     border_width=2, command=open_cert)
+importCert.place(relx=0.15, rely=0.27)
 
-importCert.place(relx=0.5, rely=0.30, anchor="center")
+# Textlabel for Certificate Image
+certLabel = CTkLabel(master=app, text="exampleCertificate.jpg", font=("Arial", 12))
+certLabel.place(relx=0.15, rely=.34)
 
-# Textlabel for Calibration Certificate image
-certLabel = CTkLabel(master=app, text="exampleCertificate.xml", font=("Arial", 12))
-certLabel.place(relx=0.5, rely=0.40, anchor="center")
+# Textlabel for Import Calibration Certificate image
+importFormatL = CTkLabel(master=app, text="Select Calibration Formal XML", font=("Arial", 12))
+importFormatL.place(relx=0.15, rely=0.44)
 
 # import button for xml
-importFormat = CTkButton(master=app, text="Select Calibration Format XML", corner_radius=15,
+importFormat = CTkButton(master=app, text="Upload File", corner_radius=15,
                     fg_color="#4158D0", hover_color="#C850C0", border_color="#FFCC70",
                     border_width=2, command=open_format)
-importFormat.place(relx=0.5, rely=0.50, anchor="center")
+importFormat.place(relx=0.15, rely=0.53)
 
 # Textlabel for Calibration Format XML
 formatLabel = CTkLabel(master=app, text="exampleFormat.xml", font=("Arial", 12))
-formatLabel.place(relx=0.5, rely=0.60, anchor="center")
+formatLabel.place(relx=0.15, rely=0.60)
 
 # export button for DCC
 exportDCC = CTkButton(master=app, text="Export DCC", corner_radius=15,
                     fg_color="#4158D0", hover_color="#C850C0", border_color="#FFCC70",
                     border_width=2, command=export_dcc)
-exportDCC.place(relx=0.5, rely=0.70, anchor="center")
+exportDCC.place(relx=0.15, rely=0.75)
 
 # Textlabel for DCC
 dccLabel = CTkLabel(master=app, text="exampleDCC.xml", font=("Arial", 12))
-dccLabel.place(relx=0.5, rely=0.80, anchor="center")
+dccLabel.place(relx=0.5, rely=0.75)
 
 app.mainloop()
 
